0eaca7646ade915d2c8aa01bcfc4c5adee83b1af
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-10-20 15:17:47 +0200

    gallery URLs updated

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -3,22 +3,22 @@ import "./style.css";
 const Gallery = () => (
     <div className="imageGallery">
     <div className="imageGallery__container"><img className="imageGallery__image"
-      src="https://raw.githubusercontent.com/bedicooper/homepage/main/img/JOT.jpg"
+      src="https://raw.githubusercontent.com/bedicooper/homepage/main/src/img/JOT.jpg"
       alt="apartment block on Joteyki street" /></div>
     <div className="imageGallery__container"><img className="imageGallery__image"
-      src="https://raw.githubusercontent.com/bedicooper/homepage/main/img/RAT.jpg"
+      src="https://raw.githubusercontent.com/bedicooper/homepage/main/src/img/RAT.jpg"
       alt="hotel on Ratibora street" /></div>
     <div className="imageGallery__container"><img className="imageGallery__image"
-      src="https://raw.githubusercontent.com/bedicooper/homepage/main/img/KOM.jpg"
+      src="https://raw.githubusercontent.com/bedicooper/homepage/main/src/img/KOM.jpg"
       alt="offices on Komandorska street" /></div>
     <div className="imageGallery__container"><img className="imageGallery__image"
-      src="https://raw.githubusercontent.com/bedicooper/homepage/main/img/PIL.jpg"
+      src="https://raw.githubusercontent.com/bedicooper/homepage/main/src/img/PIL.jpg"
       alt="hotel on Pilsudskiego street" /></div>
     <div className="imageGallery__container"><img className="imageGallery__image"
-      src="https://raw.githubusercontent.com/bedicooper/homepage/main/img/GRU.jpg"
+      src="https://raw.githubusercontent.com/bedicooper/homepage/main/src/img/GRU.jpg"
       alt="offices on Grunwaldzka street" /></div>
     <div className="imageGallery__container"><img className="imageGallery__image"
-      src="https://raw.githubusercontent.com/bedicooper/homepage/main/img/POB.jpg"
+      src="https://raw.githubusercontent.com/bedicooper/homepage/main/src/img/POB.jpg"
       alt="apartment block on Poborzanska street" /></div>
   </div>
 );
