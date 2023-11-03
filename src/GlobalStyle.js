ed258992a06eaa7bcca279fd981eebac17eb216b
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-11-03 17:58:27 +0100

    GlobalStyle.js with createGlobalStyle and theme added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+    margin: auto;
+}
+
+*,
+::after,
+::before {
+    box-sizing: inherit;
+}
+
+body {
+    max-width: 960px;
+    margin: auto;
+    padding: 0px 16px;
+    font-family: Lato, sans-serif;
+    color: ${({ theme }) => theme.text};
+    background-color: ${({ theme }) => theme.secondary};
+    word-break: break-word;
+    overflow-wrap: break-word;
+    transition: color ${({ theme }) => theme.transition}, background-color ${({ theme }) => theme.transition};
+}
+`
+export const lightTheme = {
+    primary: 'hsl(0, 0%, 85%)',
+    secondary: 'hsl(0, 0%, 75%)',
+    text: 'hsl(0, 0%, 15%)',
+    shadow: 'hsl(0, 0%, 55%)',
+    transition: '0.2s ease-in',
+    active: 'hsl(0, 0%, 50%)'
+};
+export const darkTheme = {
+    primary: 'hsl(0, 0%, 10%)',
+    secondary: 'hsl(0, 0%, 15%)',
+    text: 'hsl(0, 0%, 55%)',
+    shadow: 'hsl(0, 0%, 8%)',
+    transition: '0.2s ease-in',
+    active: 'hsl(0, 0%, 75%)'
+};
