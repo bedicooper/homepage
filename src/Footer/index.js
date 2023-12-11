90937dc215bbfa3a5ea286ee713bb58e9b54012c
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-12-11 12:41:49 +0100

    fixed typo in Footer and redundant class in Header

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,7 +1,8 @@
 import GitHubIcon from "../utils/GitHubIcon";
 import LinkedInIcon from "../utils/LinkedInIcon";
+import { StyledFooter } from "../styled";
 import { StyledForthHeading } from "./styled";
-import { StyledFooter, FooterTitleContainer, FooterSocialsContainer, FooterContentContainer, FooterAdnotation, FooterContent } from "./styled";
+import { FooterTitleContainer, FooterSocialsContainer, FooterContentContainer, FooterAdnotation, FooterContent } from "./styled";
 
 export default () => (
     <StyledFooter>
@@ -19,7 +20,7 @@ export default () => (
                 bednarz.mateusz@gmail.com
             </FooterContent>
             <FooterAdnotation>
-            I'm eager to helo You build Your web app, so feel free to reach out!
+            I'm eager to help You build Your web app, so feel free to reach out!
         </FooterAdnotation>
         </FooterContentContainer>
 
--- src/features/HomePage/index.js
+++ src/features/HomePage/index.js
@@ -3,7 +3,7 @@ import { Container } from "../styled";
 export default () => (
     <Container>
         <h1>Hi, I'm Matthew</h1>
-        <p class="header__title">Architect based in Warsaw</p>
+        <p>Architect based in Warsaw</p>
         <button class="button">✉ Hire me</button>
     </Container>
 );
