b756bd3d103c7b439df98cdbc880d0e786e2c3eb
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-02 13:07:57 +0100

    Footer refactored to flex

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,29 +1,30 @@
 import GitHubIcon from "../utils/GitHubIcon";
 import LinkedInIcon from "../utils/LinkedInIcon";
-import { StyledFooter } from "../styled";
-import { StyledForthHeading } from "./styled";
-import { FooterTitleContainer, FooterSocialsContainer, FooterContentContainer, FooterAdnotation, FooterContent } from "./styled";
+import { ForthHeading } from "../styled";
+import { StyledFooter, FooterTitleContainer, FooterSocialsContainer, FooterContentContainer, FooterAdnotation, FooterContent } from "./styled";
 
-export default () => (
-    <StyledFooter>
+const Footer = () => (
+    <>
         <FooterTitleContainer>
-            <StyledForthHeading>
+            <ForthHeading>
                 LET'S TALK!
-            </StyledForthHeading>
+            </ForthHeading>
         </FooterTitleContainer>
-        <FooterSocialsContainer>
-            <GitHubIcon />
-            <LinkedInIcon />
-        </FooterSocialsContainer>
-        <FooterContentContainer>
-            <FooterContent>
-                bednarz.mateusz@gmail.com
-            </FooterContent>
-            <FooterAdnotation>
-            I'm eager to help You build Your web app, so feel free to reach out!
-        </FooterAdnotation>
-        </FooterContentContainer>
-
-    </StyledFooter>
-
+        <StyledFooter>
+            <FooterSocialsContainer>
+                <GitHubIcon />
+                <LinkedInIcon />
+            </FooterSocialsContainer>
+            <FooterContentContainer>
+                <FooterContent>
+                    mateusz@swxmb.com
+                </FooterContent>
+                <FooterAdnotation>
+                    I'm eager to help You build Your web app, so feel free to reach out!
+                </FooterAdnotation>
+            </FooterContentContainer>
+        </StyledFooter>
+    </>
 );
+
+export default Footer;
--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -1,43 +1,35 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-    display: grid;
-    grid-template-columns: 2fr 32px 6fr;
-    grid-template-rows: 24px 76px;
-`
-
-export const StyledForthHeading = styled.h4`
-    font-weight: 200;
+    margin: auto;
+    display: flex;
+    gap: 12px;
 `
 
 export const FooterTitleContainer = styled.div`
-    grid-column: 1 / 2;
-    grid-row: 1 / 2;
-    text-align: right;
+    width: 100%;
+    text-align: left;
     height: fit-content;
     padding-right: 6px;
 `
 
 export const FooterSocialsContainer = styled.div`
-    grid-column: 2 / 3;
-    grid-row: 2 / 4;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    row-gap: 12px;
 `
 
 export const FooterContentContainer = styled.div`
-    grid-column: 3 / 4;
-    grid-row: 2 / 3;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-    padding-left: 12px;
+    row-gap: 12px;
+    justify-content: space-between;
 `
 
 export const FooterContent = styled.p`
-    font-size: 1.5rem;
+    font-size: 1.25rem;
     font-weight: 900;
+    line-height: 2rem;
     margin: 0;
 `
 
