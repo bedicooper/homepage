ad501a0b5e3b94540280fce03d38f0bf7fbe2361
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-08 13:18:12 +0100

    Page structure moved to new PersonalHomepage file in /app

---
+++ src/app/PersonalHomepage.js
@@ -0,0 +1,65 @@
+import {
+	HashRouter,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import {
+	toAbout,
+	toHome,
+	toSkills,
+	toWork
+} from './routes';
+import { TopBar } from '../common/TopBar';
+import {
+	WrapperMobile,
+	Wrapper,
+	StyledMain,
+} from './styled';
+import { Header, HeaderMobile } from '../common/Header';
+import { Footer } from '../common/Footer';
+import HomePage from '../features/HomePage';
+import PortfolioPage from '../features/PortfolioPage';
+import SkillsPage from '../features/SkillsPage';
+import AboutPage from '../features/AboutPage';
+
+export default () => (
+	<HashRouter>
+		<TopBar />
+
+		<WrapperMobile>
+			<HeaderMobile />
+			<StyledMain>
+				<AboutPage />
+				<SkillsPage />
+				<PortfolioPage />
+			</StyledMain>
+			<Footer />
+		</WrapperMobile>
+
+		<Wrapper>
+			<Header />
+			<StyledMain>
+				<Switch>
+					<Route path={toHome()}>
+						<HomePage />
+					</Route>
+					<Route path={toWork()}>
+						<PortfolioPage />
+					</Route>
+					<Route path={toSkills()}>
+						<SkillsPage />
+					</Route>
+					<Route path={toAbout()}>
+						<AboutPage />
+					</Route>
+					<Route path="/">
+						<Redirect to={toHome()} />
+					</Route>
+				</Switch>
+			</StyledMain>
+			<Footer />
+		</Wrapper>
+
+	</HashRouter>
+);
