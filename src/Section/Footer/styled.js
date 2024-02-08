74dc77c1d4ffff39345995d50e2a70ee0c5f514e
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-08 13:05:31 +0100

    old assets and files deleted

--- src/Section/Footer/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledFooter = styled.footer`
-    text-align: center;
-    font-family: 'Merriweather', serif;
-    font-size: 10px;
-    border-bottom: none;
-`
--- src/Section/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.section`
-    margin: 8px 0px;
-    padding: 16px;
-`
