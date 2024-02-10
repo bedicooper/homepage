b93cb6942b8db8e15a319bdeeeddd3603e204398
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-10 14:14:20 +0100

    filtering homepage from portfolio added

--- src/features/PortfolioPage/portfolioSaga.js
+++ src/features/PortfolioPage/portfolioSaga.js
@@ -1,11 +1,13 @@
 import { call, delay, put, takeLatest } from 'redux-saga/effects'
 import { fetchRepositories, fetchRepositoriesSuccess, fetchRepositoriesError } from './porfolioSlice'
 import { getRepositories } from '../../utils/getGithubRepositories'
+import { processGitHubData } from '../../utils/processApiData';
 
 function* fetchRepositoriesHandler({ payload: username }) {
     try {
         yield delay(600); //delay to demo the loading state
-        const repositories = yield call(getRepositories, username);
+        const rawData = yield call(getRepositories, username);
+        const repositories = yield call(processGitHubData, rawData);
         yield put(fetchRepositoriesSuccess(repositories));
     } catch (error) {
         yield put(fetchRepositoriesError());
