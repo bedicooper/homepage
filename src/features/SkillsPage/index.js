152d40224988197ff59418429d0c73e94f0c85d7
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-12 11:04:23 +0100

    addded line-height to SkillsPage Paragraphs

--- src/features/SkillsPage/index.js
+++ src/features/SkillsPage/index.js
@@ -1,5 +1,5 @@
 import { SecondHeading } from "../../app/styled";
-import { StyledSection } from "../styled";
+import { StyledSection, Paragraph } from "../styled";
 import { SkillList } from "./SkillList";
 import { skills, nextSkills } from "./skillsData"
 
@@ -8,18 +8,19 @@ export default () => (
         <SecondHeading>
             how I can help You
         </SecondHeading>
-        <p>
+        <Paragraph>
             I specialize in frontend development in JavaScript, primarily working within React framework with Router and Redux Toolkit.
             I'm also eager to explore other popular frameworks like Vue.js and Angular, as well as more backend oriented frameworks like Node.js
+            <br/>
             With a growing skill set in frontend development, I prioritize clean code and seamless user experiences. 
             I collaborate effectively and have experience in utilization of agile methodologies like 
             Scrum and tools such as Trello for efficient project management. 
             Additionally, my familiarity with Git and GitHub facilitates smooth version control and collaboration.
-        </p>
-        <p>
+        </Paragraph>
+        <Paragraph>
             In my previous role as an architect, I thrived in collaborating within multidisciplinary teams to deliver complex projects.
             I consistently took on significant responsibilities, contributing to the successful completion of various large-scale projects.
-        </p>
+        </Paragraph>
 
         <SkillList
             title={"My sklillset includes"}
--- src/features/styled.js
+++ src/features/styled.js
@@ -6,3 +6,12 @@ export const StyledSection = styled.section`
     width: 100%;
     height: auto;
 `
+
+export const Paragraph = styled.p`
+    margin: 0.5em 0 1em 2em;
+    line-height: 1.4;
+    
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        margin-left: 1.25em;
+    }
+`
